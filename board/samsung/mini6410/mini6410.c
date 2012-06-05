92f536d10a00ba994bbbe608295e78187c0ce6e2
liangxiaoju <liangxiaoju@gmail.com>
2012-06-06 00:43:14 +0800

    net: add dm9000 support.
    support for nfs and tftp boot.

--- board/samsung/mini6410/mini6410.c
+++ board/samsung/mini6410/mini6410.c
@@ -37,13 +37,13 @@
 DECLARE_GLOBAL_DATA_PTR;
 
 /* ------------------------------------------------------------------------- */
-#define CS8900_Tacs	0x0	/* 0clk		address set-up		*/
-#define CS8900_Tcos	0x4	/* 4clk		chip selection set-up	*/
-#define CS8900_Tacc	0xE	/* 14clk	access cycle		*/
-#define CS8900_Tcoh	0x1	/* 1clk		chip selection hold	*/
-#define CS8900_Tah	0x4	/* 4clk		address holding time	*/
-#define CS8900_Tacp	0x6	/* 6clk		page mode access cycle	*/
-#define CS8900_PMC	0x0	/* normal(1data)page mode configuration	*/
+#define DM9000_Tacs	0x0	/* 0clk		address set-up		*/
+#define DM9000_Tcos	0x4	/* 4clk		chip selection set-up	*/
+#define DM9000_Tacc	0xE	/* 14clk	access cycle		*/
+#define DM9000_Tcoh	0x1	/* 1clk		chip selection hold	*/
+#define DM9000_Tah	0x4	/* 4clk		address holding time	*/
+#define DM9000_Tacp	0x6	/* 6clk		page mode access cycle	*/
+#define DM9000_PMC	0x0	/* normal(1data)page mode configuration	*/
 
 static inline void delay(unsigned long loops)
 {
@@ -56,13 +56,13 @@ static inline void delay(unsigned long loops)
  * Miscellaneous platform dependent initialisations
  */
 
-static void cs8900_pre_init(void)
+static void dm9000_pre_init(void)
 {
 	SROM_BW_REG &= ~(0xf << 4);
 	SROM_BW_REG |= (1 << 7) | (1 << 6) | (1 << 4);
-	SROM_BC1_REG = ((CS8900_Tacs << 28) + (CS8900_Tcos << 24) +
-			(CS8900_Tacc << 16) + (CS8900_Tcoh << 12) +
-			(CS8900_Tah << 8) + (CS8900_Tacp << 4) + CS8900_PMC);
+	SROM_BC1_REG = ((DM9000_Tacs << 28) + (DM9000_Tcos << 24) +
+			(DM9000_Tacc << 16) + (DM9000_Tcoh << 12) +
+			(DM9000_Tah << 8) + (DM9000_Tacp << 4) + DM9000_PMC);
 }
 
 static int s3c_otg_phy_control(int on)
@@ -92,7 +92,7 @@ int board_late_init(void)
 
 int board_init(void)
 {
-	cs8900_pre_init();
+	dm9000_pre_init();
 
 	/* NOR-flash in SROM0 */
 
@@ -142,9 +142,11 @@ ulong virt_to_phy_smdk6400(ulong addr)
 int board_eth_init(bd_t *bis)
 {
 	int rc = 0;
-#ifdef CONFIG_CS8900
-	rc = cs8900_initialize(0, CONFIG_CS8900_BASE);
+
+#if defined(CONFIG_DRIVER_DM9000)
+	rc = dm9000_initialize(bis);
 #endif
+
 	return rc;
 }
 #endif
